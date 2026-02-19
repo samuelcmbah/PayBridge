acd3ca941e342e34dae282fa35658daa0f5487e1
Samuel Mbah <samuelcmbah@gmail.com>
2026-02-19 17:53:27 +0100

    add pending migrations

---
+++ PayBridge.Infrastructure/Migrations/20260219164909_InitialCreate.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PayBridge.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Payments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Reference = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Provider = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Status = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    Purpose = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    Currency = table.Column<string>(type: "character varying(3)", maxLength: 3, nullable: false),
+                    ExternalUserId = table.Column<string>(type: "character varying(254)", maxLength: 254, nullable: false),
+                    AppName = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    ExternalReference = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    RedirectUrl = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    NotificationUrl = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    VerifiedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payments", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_Reference",
+                table: "Payments",
+                column: "Reference",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Payments");
+        }
+    }
+}
