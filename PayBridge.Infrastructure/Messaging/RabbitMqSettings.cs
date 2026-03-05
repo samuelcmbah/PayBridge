59a2b1105b71bf0d9eccd29ef25dd921adf40ddb
Samuel Mbah <samuelcmbah@gmail.com>
2026-03-05 19:20:31 +0100

    add strongly typed settings class for rabbitMq

---
+++ PayBridge.Infrastructure/Messaging/RabbitMqSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace PayBridge.Infrastructure.Messaging
+{
+    public record RabbitMqSettings
+    {
+        public string Host { get; init; } = default!;
+        public int Port { get; init; }
+        public string VirtualHost { get; init; } = default!;
+        public string Username { get; init; } = default!;
+        public string Password { get; init; } = default!;
+    }
+}
